3268b7e813f345abd9ac0a0ae6f5027572d78b44
aleksandr-a11y <117071816+aleksandr-a11y@users.noreply.github.com>
2024-10-01 11:09:12 +0300

    Update main2_2.py

--- main2_2.py
+++ main2_2.py
@@ -3,6 +3,6 @@ second = int(input("enter the second number: "))
 third = int(input("enter the third number: "))
 if first == second == third :
     print('3')
-elif first == second or first == third or second == third :
+elif first == second and first == third and second == third :
     print('2')
 else: print('0')
